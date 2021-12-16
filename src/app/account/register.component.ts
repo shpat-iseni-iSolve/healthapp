8e984131e73eb927895994b2862a6a7c4395d255
Alban11 <ibraimialban14@gmail.com>
2021-12-16 14:42:22 +0100

    Commit: log in and register ui update

--- src/app/account/register.component.ts
+++ src/app/account/register.component.ts
@@ -28,7 +28,10 @@ export class RegisterComponent implements OnInit {
         this.form = this.formBuilder.group({
             firstName: ['', Validators.required],
             lastName: ['', Validators.required],
+            guardianName: ['', Validators.required],
+            socialID: [0, Validators.required],            
             username: ['', Validators.required],
+            email: ['', Validators.required],
             password: ['', [Validators.required, Validators.minLength(6)]]
         });
     }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { JwtInterceptor, ErrorInterceptor } from './_helpers';
 import { AppComponent } from './app.component';
 import { AlertComponent } from './_components';
 import { HomeComponent } from './home';
+
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
     imports: [
         BrowserModule,
         ReactiveFormsModule,
         HttpClientModule,
+        FormsModule,
+        RouterModule,
         AppRoutingModule
     ],
     declarations: [
         AppComponent,
         AlertComponent,
-        HomeComponent
+        HomeComponent,
+
     ],
     providers: [
         { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
